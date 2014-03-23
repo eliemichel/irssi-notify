3a311cc95525b00492148cd585f622b702b9a896
Élie Michel <elie.michel@ens.fr>
2014-03-23 16:48:53 +0100

    Python version of the server

--- server.py
+++ server.py
@@ -1,10 +1,49 @@
-from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
-import SocketServer
+#!/usr/bin/env python3
 
-PORT = 9872
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import urlparse, parse_qs
+from subprocess import call
 
-print ("Serving at port %d" % (PORT,))
-httpd.serve_forever()
+PORT = 9871
+
+
+def notif_filter(q):
+	return \
+		'info' in q and \
+		'- Nouveau message de pfcbot -' not in q['info']
+
+def notif_escape(msg):
+	return msg.replace('\\', '\\\\')
+
+
+class Handler(BaseHTTPRequestHandler):
+	def do_GET(s):
+		req = urlparse(s.path)
+		q = parse_qs(req.query)
+		print(req.path)
+		if req.path == '/notify':
+			if notif_filter(q):
+				call(['notify-send', 'IRC', notif_escape(q['info'][0])])
+			else:
+				print("Muted request")
+		s.send_response(200)
+		s.send_header("Content-type", "text/plain")
+		s.end_headers()
+		print()
+
+
+def main():
+	httpd = HTTPServer(('localhost', PORT), Handler)
+	print("Serving at port %d" % (PORT,))
+	try:
+		httpd.serve_forever()
+	except KeyboardInterrupt:
+		pass
+	httpd.server_close()
+	print("\nServer closed")
+
+
+if __name__ == '__main__':
+	main()
 
 
